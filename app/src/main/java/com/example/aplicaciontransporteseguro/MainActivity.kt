a13b39629bc0b67e4412781f2bcea6a8be3f402a
HCfercho <carferchomen@gmail.com>
2019-07-27 19:27:49 -0500

    clase Usuario creada

--- app/src/main/java/com/example/aplicaciontransporteseguro/MainActivity.kt
+++ app/src/main/java/com/example/aplicaciontransporteseguro/MainActivity.kt
@@ -8,5 +8,19 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val json = """
+            {
+              "denuncias": [],
+              "createdAt": 1564272729328,
+              "updatedAt": 1564272729328,
+              "id": 1,
+              "correo_electronico": "",
+              "nombre_completo": "",
+              "nombre_usuario": "",
+              "contrasena": "",
+              "fkTipoUsuario": null
+            }
+        """.trimIndent()
     }
 }
---
+++ app/src/main/java/com/example/aplicaciontransporteseguro/Usuario.kt
@@ -0,0 +1,4 @@
+package com.example.aplicaciontransporteseguro
+
+class Usuario {
+}
